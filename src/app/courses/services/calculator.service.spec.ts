18c40b800600a57b1cd2e1449d0c3a2a3e60bde0
ajulthomas <ajul.thomas@outlook.com>
2021-05-31 10:59:37 +0530

    adds Angular TestBed to manage dependencies

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -1,10 +1,18 @@
+import { LoggerService } from './logger.service';
+import { TestBed } from '@angular/core/testing';
 import { CalculatorService } from './calculator.service';
 describe('CalculatorService', () => {
     let calculator: CalculatorService,
-        loggerSpy: any;
-    beforeEach(function initiate() {
+        loggerSpy: jasmine.SpyObj<LoggerService>;
+    beforeEach(() => {
         loggerSpy = jasmine.createSpyObj('LoggerService', ['log']);
-        calculator = new CalculatorService(loggerSpy);
+        TestBed.configureTestingModule({
+            providers: [
+                CalculatorService,
+                { provide: LoggerService, useValue: loggerSpy }
+            ]
+        });
+        calculator = TestBed.inject(CalculatorService);
     });
     it('should add two numbers', () => {
         const result = calculator.add(2, 2);
